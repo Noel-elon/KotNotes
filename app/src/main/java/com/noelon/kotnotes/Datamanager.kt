61ec3ef74ec1d4f36fa53324fef79749c02162ba
Noel-elon <noelnwaelugo@gmail.com>
2020-04-06 20:34:32 +0100

    Data manager2

--- app/src/main/java/com/noelon/kotnotes/Datamanager.kt
+++ app/src/main/java/com/noelon/kotnotes/Datamanager.kt
@@ -3,4 +3,21 @@ package com.noelon.kotnotes
 class Datamanager {
     val courses = HashMap<String, CourseInfo>()
     val notes = ArrayList<NoteInfo>()
+
+
+
+    init {
+        initializecourses()
+    }
+
+    private fun initializecourses(){
+        var course = CourseInfo("Android_Intents", "Android programming with intents")
+        courses.set(course.coursename, course)
+
+        course = CourseInfo(coursename = "android_async", coursetitle = "Android Async programming and services")
+        courses.set(course.coursename, course)
+
+        course = CourseInfo(coursetitle = "Java Fundamnetals", coursename = "java_funds")
+        courses.set(course.coursename, course)
+    }
 }
