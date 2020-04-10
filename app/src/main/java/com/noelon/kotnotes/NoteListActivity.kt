1341d9daba86bc9c133e127f29ef1dfad545a43b
Noel-elon <noelnwaelugo@gmail.com>
2020-04-10 14:29:30 +0100

    recyclerview

--- app/src/main/java/com/noelon/kotnotes/NoteListActivity.kt
+++ app/src/main/java/com/noelon/kotnotes/NoteListActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.ArrayAdapter
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 
 import kotlinx.android.synthetic.main.activity_note_list.*
 import kotlinx.android.synthetic.main.content_note_list.*
@@ -21,20 +22,16 @@ class NoteListActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        noteList.adapter =
-            ArrayAdapter<NoteInfo>(this, android.R.layout.simple_list_item_1, Datamanager.notes)
+        recycler_list.layoutManager = LinearLayoutManager(this)
 
-        noteList.setOnItemClickListener { parent, view, position, id ->
-            val intent2 = Intent(this, MainActivity::class.java)
-            intent2.putExtra(extranoteposition, position)
-            startActivity(intent2)
-        }
+
+
+
 
     }
 
     override fun onResume() {
         super.onResume()
-        (noteList.adapter as ArrayAdapter<NoteInfo>).notifyDataSetChanged()
     }
 
 }
